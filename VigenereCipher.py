eaa0cd31ed055063439befb68b66c7eac6128cee
unknown <templanzatintin@gmail.com>
2023-04-18 22:00:33 +0800

    Greeting

---
+++ VigenereCipher.py
@@ -0,0 +1,9 @@
+# Templanza, Kristine Joy F.
+# BSCPE 1-4
+# Assignment No.2 - Problem 3
+
+# Greeting and border line
+print("")
+print("\033[35m※ \033[0m" * 35)
+print("")
+print("\033[45m ♥ Welcome to Vigenère Cipher! ♥ \033[0m".center(78, "-"))
