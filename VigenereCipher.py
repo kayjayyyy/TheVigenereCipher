3be3f7619cefcfbedfd1d30c85b35f94cfb0f7ab
unknown <templanzatintin@gmail.com>
2023-04-18 22:33:16 +0800

    User's input is working

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -7,3 +7,10 @@ print("")
 print("\033[35m※ \033[0m" * 35)
 print("")
 print("\033[45m ♥ Welcome to Vigenère Cipher! ♥ \033[0m".center(78, "-"))
+
+# Ask the name of the user
+name = input("\n\033[3mGood day! What is your name? \033[0m")
+print("\n\033[3;33mI hope you are doing well,", name + "!\033[0m")
+print("")
+
+print("\033[36m Let's get started! \033[0m".center(78, "~"))
